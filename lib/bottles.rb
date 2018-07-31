bf5cdd8fa6c3b69989f91e4f9aaed5763b6b73c7
Remy Abdullahi <abdu0299@umn.edu>
2018-07-31 12:37:48 -0500

    Make "1" branch redundant, add quantity method
    Identify and add quantity method, used flocking rules to get there.
    Something to note from the book:
    
    ```
    When the else branch is implemented first, :FIXME can always be used for the default. This not only saves you from having to figure out the right value, it also serves as a reminder to remove this temporary default later. If the non- else branch is implemented first, the default has to be set to something that actually meets the condition and so makes the true branch execute. Therefore, implementing the non- else branch first places a slightly greater burden on you. You have to use a specific, real value for the default, and then remember to remove the default once the transition is complete.
    ```
    
    bookmark: 4.5 on page 87

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,20 +11,23 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "No more bottles of beer on the wall, " +
+        "No more bottles of beer on the wall, " +
         "no more bottles of beer.\n" +
         "Go to the store and buy some more, " +
         "99 bottles of beer on the wall.\n"
-    when 1
-      "#{number} #{container(number)} of beer on the wall, " +
-        "#{number} #{container(number)} of beer.\n" +
-        "Take #{pronoun(number)} down and pass it around, " +
-        "no more #{container(number - 1)} of beer on the wall.\n"
     else
-      "#{number} #{container(number)} of beer on the wall, " +
+        "#{number} #{container(number)} of beer on the wall, " +
         "#{number} #{container(number)} of beer.\n" +
         "Take #{pronoun(number)} down and pass it around, " +
-        "#{number-1} #{container(number - 1)} of beer on the wall.\n"
+        "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number
     end
   end
 
